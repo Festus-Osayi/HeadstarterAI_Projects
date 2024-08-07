cee373d837bf03448404e6c6b15b98d055ac49f4
Festus <fosayi@myseneca.ca>
2024-08-06 23:12:36 -0400

    Added a bit of modification on the nav bar

--- inventory-management/components/MainNav.jsx
+++ inventory-management/components/MainNav.jsx
@@ -69,51 +69,55 @@ function MainNav() {
             Pantry Tracker
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pagesLink.map((link) => (
-                <MenuItem key={link} onClick={handleCloseNavMenu}>
-                  <NextLink
-                    textAlign="center"
-                    href={`${link}`}
-                    passHref
-                    legacyBehavior
-                  >
-                    <Link className="no-underline text-black">
-                      {link === "/" ? "Home" : "Join"}
-                    </Link>
-                  </NextLink>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          {!auth.currentUser && (
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pagesLink.map((link) => (
+                  <MenuItem key={link} onClick={handleCloseNavMenu}>
+                    <NextLink
+                      textAlign="center"
+                      href={`${link}`}
+                      passHref
+                      legacyBehavior
+                    >
+                      <Link className="no-underline text-black">
+                        {link === "/" ? "Home" : "Join"}
+                      </Link>
+                    </NextLink>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          )}
+
           <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           <Typography
             variant="h5"
@@ -192,10 +196,9 @@ function MainNav() {
                     <Link className="text-gray-800 no-underline">New item</Link>
                   </NextLink>
 
-                  <Button 
-                  onClick={signOut}
-                  variant="contained"
-                  >Logout</Button>
+                  <Button onClick={signOut} variant="contained">
+                    Logout
+                  </Button>
                 </MenuItem>
               </Menu>
             </Box>
